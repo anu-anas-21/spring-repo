d5de508bfc5943599d65a78bea5484e524678cdb
anu-anas-21 <anasanu0174@gmail.com>
2025-06-22 13:40:20 +0530

    Initial One

---
+++ DependencyInjection/src/main/java/com/example/di/Customers.java
@@ -0,0 +1,26 @@
+package com.example.di;
+
+public class Customers {
+	private int custId;
+	private String custName;
+	private String courseName;
+	
+	public int getCusId() {
+		return custId;
+	}
+	public void setCusId(int custId) {
+		this.custId = custId;
+	}
+	public String getCustName() {
+		return custName;
+	}
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+}
